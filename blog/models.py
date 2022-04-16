9112cde888bfccb1dd24e5895fc96050644f683b
ravil200 <raviakylbekov08@gmail.com>
2022-04-16 19:14:37 +0600

    added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class AboutUs(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name = 'About Us'
+        verbose_name_plural = 'About Us'
+
+class Trainer(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self) -> str:
+        return self.name 
+
+class Classes(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+class Contact(models.Model):
+    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
